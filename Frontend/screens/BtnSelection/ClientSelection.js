3049e1fa265fc432b71b0d2277e75b808cfa0953
Gabox04 <antonio62697@gmail.com>
2022-12-12 16:43:35 -0400

    Create customer selection buttons

--- Frontend/screens/BtnSelection/ClientSelection.js
+++ Frontend/screens/BtnSelection/ClientSelection.js
@@ -1,32 +1,183 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Pressable,
+} from "react-native";
 import Header from "../../components/UI/Header";
 import Colors from "../../constants/Colors/Colors";
 
+// import NavBtn from "../../components/UI/NavBtns";
+
 function ClientSelection({ navigation }) {
+  // function navNext() {
+  //   if (toggleNewCustomer) navigation.navigate("ClientInformation");
+  //   else if (toggleExistingCustomer) navigation.navigate("ExistingClient");
+  // }
+  // function navJobOrder() {
+  //   navigation.navigate("JobOrderMain");
+  // }
+
+  const [toggleNewCustomer, setToggleNewCustomer] = useState(false);
+  const [toggleExistingCustomer, setToggleExistingCustomer] = useState(false);
+
+  let NewCustomerIcon = require("../../assets/images/NewCustomerIcon.png");
+  let NewCustomerSelectedIcon = require("../../assets/images/NewCustomerSelectedIcon.png");
+  let ExistingCustomerIcon = require("../../assets/images/ExistingCustomerIcon.png");
+  let ExistingCustomerSelectedIcon = require("../../assets/images/ExistingCustomerSelectedIcon.png");
+
+  function NewCustomer() {
+    if (toggleNewCustomer === true) {
+      return (
+        <View style={[styles.ButtonPressed, { paddingHorizontal: 65 }]}>
+          <Image
+            style={{
+              position: "relative",
+              width: 135,
+              height: 172,
+              marginBottom: 30,
+            }}
+            source={NewCustomerSelectedIcon}
+          />
+          <Text style={{ fontSize: 50, color: Colors.white }}>
+            New Customer
+          </Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={[styles.Buttons, { paddingHorizontal: 65 }]}>
+          <Image
+            style={{
+              position: "relative",
+              width: 135,
+              height: 172,
+              marginBottom: 30,
+            }}
+            source={NewCustomerIcon}
+          />
+          <Text style={{ fontSize: 50 }}>New Customer</Text>
+        </View>
+      );
+    }
+  }
+
+  function ExistingCustomer(toggleBtn2) {
+    if (toggleBtn2 === true) {
+      return (
+        <View
+          style={[
+            styles.ButtonPressed,
+            { paddingHorizontal: 30, paddingVertical: 80 },
+          ]}
+        >
+          <Image
+            style={{
+              position: "relative",
+              width: 184,
+              height: 136,
+              marginBottom: 30,
+            }}
+            source={ExistingCustomerSelectedIcon}
+          />
+          <Text style={{ fontSize: 50, color: Colors.white }}>
+            Existing Customer
+          </Text>
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={[
+            styles.Buttons,
+            { paddingHorizontal: 30, paddingVertical: 80 },
+          ]}
+        >
+          <Image
+            style={{
+              position: "relative",
+              width: 184,
+              height: 136,
+              marginBottom: 30,
+            }}
+            source={ExistingCustomerIcon}
+          />
+          <Text style={{ fontSize: 50 }}>Existing Customer</Text>
+        </View>
+      );
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Header divideH={8} divideW={1.1} colorHeader={Colors.yellowDark}>
-        <Text style={styles.title}>Select client for Job Order</Text>
+        <View style={styles.HeaderContent}>
+          <Text style={styles.title}>Select customer for Job Order</Text>
+        </View>
       </Header>
-      <View style={styles.body}>
-        <Text>ClientSelection</Text>
+
+      <View style={styles.ButtonsAlignment}>
+        <Pressable
+          onPress={() => [
+            NewCustomer(),
+            setToggleNewCustomer(!toggleNewCustomer),
+            setToggleExistingCustomer(false),
+          ]}
+        >
+          <View>{NewCustomer(toggleNewCustomer)}</View>
+        </Pressable>
+        <Pressable
+          onPress={() => [
+            ExistingCustomer(),
+            setToggleExistingCustomer(!toggleExistingCustomer),
+            setToggleNewCustomer(false),
+          ]}
+        >
+          <View>{ExistingCustomer(toggleExistingCustomer)}</View>
+        </Pressable>
       </View>
+      {/* <View style={{ flexDirection: "row" }}>
+        <NavBtn choice={"Cancel"} nav={navJobOrder} />
+        <NavBtn choice={"Next"} nav={navNext} />
+      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  HeaderContent: {
+    justifyContent: "center",
+    width: "100%",
+    alignItems: "center",
+  },
   title: {
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: "700",
   },
-  container: {
-    flex: 1,
-    justifyContent: "center",
+  ButtonsAlignment: {
+    top: 200,
+    width: "100%",
+    alignItems: "center",
   },
-  body: {
-    zIndex: -1,
+  Buttons: {
+    margin: 13,
+    borderColor: "#cccccc",
+    borderWidth: 4,
+    borderRadius: 30,
+    alignItems: "center",
+    paddingVertical: 50,
+  },
+  ButtonPressed: {
+    margin: 13,
+    borderColor: Colors.black,
+    backgroundColor: Colors.black,
+    borderWidth: 4,
+    borderRadius: 30,
+    alignItems: "center",
+    paddingVertical: 50,
   },
 });
 
