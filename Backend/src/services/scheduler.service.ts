14d836c1e1817dcffa79084a7a84c7943239d17e
JanMichaelSe <janm.montalvo@gmail.com>
2023-01-01 09:04:46 -0400

    Updated Scheduler to remove logs

--- Backend/src/services/scheduler.service.ts
+++ Backend/src/services/scheduler.service.ts
@@ -11,16 +11,12 @@ function scheduleJobs() {
 
 function activateTemporaryAdminsDaily() {
   cron.schedule("* * * * 0-7", async () => {
-    console.log("-------------------");
-    console.log("Running Activate Temporary Admins Daily");
     await updateTemporaryAdminsByStartDate();
   });
 }
 
 function deactivateTemporaryAdminsDaily() {
   cron.schedule("* * * * 0-7", async () => {
-    console.log("-------------------");
-    console.log("Running Deactivate Temporary Admins Daily");
     await updateTemporaryAdminsByEndDate();
   });
 }
