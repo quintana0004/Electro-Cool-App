66841f56ccc065946f96ff9ca950a6c77b11d983
Jessica Quintana <jessynquintana@gmail.com>
2023-02-20 20:58:02 -0400

    Connected the search and started the create

--- Frontend/screens/JobOrders.js
+++ Frontend/screens/JobOrders.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import Header from "../components/UI/Header";
 import MenuDropDown from "../components/Navigation/MenuDropDown";
 import Colors from "../constants/Colors/Colors";
+import SearchBar from "../components/UI/SearchBar";
 
 function JobOrders({ navigation }) {
+  const [search, setSearch] = useState("");
+  const [activeCategory, setActiveCategory] = useState("JobOrders");
+  const [filters, setFilters] = useState({
+    New: false,
+    Working: false,
+    Complete: false,
+    Canceled: false,
+    Heavy: false,
+    Light: false,
+  });
+
   //Function of the btns
   function navSelectClient() {
     navigation.navigate("CustomerSelection");
@@ -22,17 +34,33 @@ function JobOrders({ navigation }) {
     navigation.navigate("CompanyPolicy");
   }
 
+  function updateSearchTerm(term) {
+    setSearch(term);
+  }
+
   return (
     <View style={styles.container}>
-      <Header divideH={8} divideW={1} colorHeader={Colors.darkBlack}>
+      <Header divideH={7} divideW={1} colorHeader={Colors.darkBlack}>
         <MenuDropDown />
+        <View style={styles.searchContainer}>
+          <SearchBar
+            onSearch={updateSearchTerm}
+            widthBar={300}
+            heightBar={50}
+            placeholderText="Search by ID or Name"
+          />
+        </View>
+        <View style={styles.btnCreateContainer}>
+          <Button
+            onPress={navSelectClient}
+            title="Create"
+            color={Colors.yellowDark}
+            style={styles.btnCreate}
+          />
+        </View>
       </Header>
       <View style={styles.body}>
-        <Text>JobOrder Screen!</Text>
-        <Button title="Gabbox01" onPress={() => navSelectClient()} />
-        <Button title="Gabbox02" onPress={() => navSelectCar()} />
-        <Button title="Shelly01" onPress={() => navClientInformation()} />
-        <Button title="Shelly02" onPress={() => navCompanyPolicy()} />
+        <Text>Job Orders</Text>
       </View>
     </View>
   );
@@ -49,6 +77,13 @@ const styles = StyleSheet.create({
   btn: {
     marginVertical: 20,
   },
+  searchContainer: {
+    flex: 1,
+    justifyContent: "center",
+    marginLeft: 35,
+    marginTop: 10,
+  },
+  btnCreate: {},
 });
 
 export default JobOrders;
