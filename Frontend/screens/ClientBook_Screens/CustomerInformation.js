99201114ff5924d557aca314d8a967c2180b4ba2
Jessica Quintana <jessynquintana@gmail.com>
2023-03-18 18:19:45 -0400

    New Stack Navigation

---
+++ Frontend/screens/ClientBook_Screens/CustomerInformation.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function CustomerInformation() {
+  return (
+    <View>
+      <Text>Customer Information</Text>
+    </View>
+  );
+}
+
+export default CustomerInformation;
