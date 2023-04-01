7aa5ba00b571eb84eff0be824df0210373f54416
JanMichaelSe <janm.montalvo@gmail.com>
2023-04-01 08:10:45 -0400

    New endpoint to get deposits by invoice id. And fixed some typos.

--- Backend/src/utils/validators.utils.ts
+++ Backend/src/utils/validators.utils.ts
@@ -3,7 +3,7 @@ import { IInvoice, IInvoiceItem, ICar, IDeposit, IAppointment, ICustomer, ITask 
 import { findCarById } from "../models/cars.model";
 import { findCompanyById } from "../models/company.model";
 import { findCustomerById } from "../models/customers.model";
-import { findDespositById } from "../models/deposits.model";
+import { findDepositById } from "../models/deposits.model";
 import { findAppointmentById } from "../models/appointments.model";
 import { findTaskById } from "../models/tasks.model";
 import { findUserById } from "../models/users.model";
@@ -118,14 +118,14 @@ async function isValidInvoiceId(id: number | string) {
   return true;
 }
 
-async function isValidDespositId(id: number | string) {
+async function isValidDepositId(id: number | string) {
   id = Number(id);
   if (isNaN(id)) {
     return false;
   }
 
-  const doesDespositExist = await findDespositById(id);
-  if (!doesDespositExist) {
+  const doesDepositExist = await findDepositById(id);
+  if (!doesDepositExist) {
     return false;
   }
 
@@ -311,7 +311,7 @@ export {
   isValidCarId,
   isValidJobOrderId,
   isValidInvoiceId,
-  isValidDespositId,
+  isValidDepositId,
   isValidAppointmentId,
   isValidTaskId,
   hasRequiredUserFields,
