4aefeac36fe2677e39ddb58d47343379df9eddc4
Gabox04 <antonio62697@gmail.com>
2023-04-01 14:22:08 -0400

    Create Btns of cars of client book

--- Frontend/components/ClientBook CBI/CarsItemClientBook.js
+++ Frontend/components/ClientBook CBI/CarsItemClientBook.js
@@ -1,70 +1,66 @@
 import { Text, View, StyleSheet } from "react-native";
-import { format } from "date-fns";
-import Status from "../Invoices/Status";
-import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import { Avatar } from "react-native-paper";
 import { useNavigation } from "@react-navigation/core";
 
-function CarItemCB({ itemData, category }) {
+import Figures from "../../constants/figures/Figures";
+import Colors from "../../constants/Colors/Colors";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+
+function CarItemCB({ itemData }) {
   const {
-    vehicleId,
-    vehicleBrand,
-    vehicleModel,
-    vehicleLecinsePlate,
-    vehicleColor,
-    vehicleYear,
-    vehicleMilage,
-    vehicleVinNumber,
-    VehicleDate,
+    ID,
+    brand,
+    model,
+    licensePlate,
+    color,
+    year,
+    mileage,
+    vinNumber,
+    date,
   } = itemData;
 
-  const navigation = useNavigation();
-
-  function DateText() {
-    return format(new Date(date), "MM/dd/yyyy");
+  function showCarInfo() {
+    console.log("Bryan es Pato pero mas PATO SOY YO");
   }
 
-  // function navigateToDetailScreen(itemId) {
-  //   if (category === "Invoices") {
-  //     navigation.navigate("InvoiceDetail", { itemId });
-  //   } else {
-  //     navigation.navigate("DepositDetail", { itemId });
-  //   }
-  // }
-
   return (
-    <View>
-      <Pressable>
-        <View style={styles.container}>
-          <Text> fcfcfcfc fhgckiyhg </Text>
+    <View style={styles.container}>
+      <Pressable style={{ margin: 10, width: 200 }} onPress={showCarInfo}>
+        <View style={{ flexDirection: "column", alignItems: "center" }}>
+          <Avatar.Icon
+            size={60}
+            icon={Figures.ClientBookEachCar}
+            color="black"
+            style={{ backgroundColor: "#D9D9D9" }}
+          />
+          <View style={{ width: "100%", alignItems: "center", margin: 7 }}>
+            <Text
+              style={{
+                fontWeight: "700",
+                fontSize: 20,
+              }}
+            >
+              {brand}, {model}
+            </Text>
+          </View>
+
+          <Text style={{ fontWeight: "600", fontSize: 17 }}>{year}</Text>
+          <Text
+            style={{
+              fontWeight: "700",
+              fontSize: 17,
+              backgroundColor: "black",
+              color: "white",
+              paddingHorizontal: 20,
+              paddingVertical: 3,
+              borderRadius: 10,
+            }}
+          >
+            {licensePlate}
+          </Text>
         </View>
       </Pressable>
     </View>
-
-    /* 
-     <View>
-       <Pressable onPress={navigateToDetailScreen.bind(this, id)}>
-         <View style={styles.container}>
-           <View style={{ width: 80 }}>
-             <Text style={[{ textAlign: "center" }, styles.boldText]}>{id}</Text>
-           </View>
-           <View style={{ width: 150 }}>
-             <Text style={styles.boldText}>
-               {lastName}, {firstName}
-             </Text>
-           </View>
-           <View style={{ width: 80 }}>
-             <Text style={styles.boldText}>{DateText()}</Text>
-           </View>
-           <View style={{ width: 80, marginLeft: 55 }}>
-             <Text style={styles.boldText}>${amountTotal}</Text>
-           </View>
-           <View style={{ width: 80, marginLeft: 30 }}>
-             <Status parentTextStyles={styles.boldText} status={status} />
-           </View>
-         </View>
-       </Pressable>
-     </View>
-    */
   );
 }
 
@@ -72,17 +68,11 @@ export default CarItemCB;
 
 const styles = StyleSheet.create({
   container: {
-    top: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    height: 50,
-    borderWidth: 1.8,
-    borderColor: "rgba(0, 0, 0, 0.3)",
+    borderWidth: 2,
     borderRadius: 10,
-    marginHorizontal: 10,
-    marginBottom: 5,
-  },
-  boldText: {
-    fontWeight: "bold",
+    borderColor: "rgba(0, 0, 0, 0.25)",
+    backgroundColor: "white",
+    elevation: 5,
+    margin: 15,
   },
 });
