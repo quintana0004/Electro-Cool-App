ab9f29bffb9f9d3f941151b260b02ff49d6e8db8
Juan <jctheman1122@gmail.com>
2023-04-22 17:50:29 -0400

    made progress

--- Frontend/screens/Dashboard.js
+++ Frontend/screens/Dashboard.js
@@ -43,18 +43,22 @@ function Dashboard({ navigation }) {
             HeightIcon={50}
             WidthIcon={50}
             Choice={1}
+            SecondTextSize={55}
           />
           {
             //Multiple buttons area
           }
           <DashboardTables
-            testfigure={Figures.Vehicle}
-            FirstText={<Text>Cars Pending Confirmation</Text>}
-            SecondText={<Text>Invoice</Text>}
+            testfigure={Figures.VehicleInShop}
+            FirstText={<Text>Vehicles Not Started</Text>}
+            SecondText={<Text>38</Text>} //editar esta variables para poner otra adicional para que haga update desde aca la data
             HeightBig={120}
             WidthBig={143}
             MarginTableTop={15}
             Choice={2}
+            SecondTextSize={40}
+            HeightIcon={52}
+            WidthIcon={120}
           />
 
           <DashboardTables
@@ -64,6 +68,7 @@ function Dashboard({ navigation }) {
             HeightIcon={52}
             WidthIcon={50}
             Choice={1}
+            SecondTextSize={55}
           />
           {
             //Remember tienes que cambiar las variables de los styles to make it work correctly
@@ -78,44 +83,52 @@ function Dashboard({ navigation }) {
             HeightIcon={45}
             WidthIcon={65}
             Choice={1}
+            SecondTextSize={55}
           />
           {
             //Another view in order to allow scrollview to work as pages instead of scrolling normally.
           }
-          <View style={styles.containerHeader1}>
-            <DashboardTables
-              testfigure={Figures.NewIconDashboard}
-              FirstText={<Text>New Vehicles Received Today</Text>}
-              SecondText={<Text>150</Text>}
-              HeightIcon={52}
-              WidthIcon={50}
-              Choice={1}
-            />
-            <DashboardTables
-              testfigure={Figures.NewIconDashboard}
-              FirstText={<Text>New Vehicles Received Today</Text>}
-              SecondText={<Text>150</Text>}
-              HeightIcon={52}
-              WidthIcon={50}
-              Choice={1}
-            />
-            <DashboardTables
-              testfigure={Figures.NewIconDashboard}
-              FirstText={<Text>New Vehicles Received Today</Text>}
-              SecondText={<Text>150</Text>}
-              HeightIcon={52}
-              WidthIcon={50}
-              Choice={1}
-            />
-            <DashboardTables
-              testfigure={Figures.NewIconDashboard}
-              FirstText={<Text>New Vehicles Received Today</Text>}
-              SecondText={<Text>150</Text>}
-              HeightIcon={52}
-              WidthIcon={50}
-              Choice={1}
-            />
-          </View>
+
+          <DashboardTables
+            testfigure={Figures.MoneyHand}
+            FirstText={<Text>Total Amount in Paid Today</Text>}
+            SecondText={<Text>$20,541.45</Text>}
+            HeightIcon={82}
+            WidthIcon={80}
+            Choice={5}
+            SecondTextSize={20}
+            ThirdText={<Text>AMT</Text>}
+            FourthText={<Text>48</Text>}
+          />
+          <DashboardTables
+            testfigure={Figures.totalDraft}
+            FirstText={<Text>Total Amount in Drafts</Text>}
+            SecondText={<Text>150</Text>}
+            HeightIcon={52}
+            WidthIcon={50}
+            Choice={1}
+            SecondTextSize={55}
+          />
+          <DashboardTables
+            testfigure={Figures.totalAmountPending}
+            FirstText={<Text>Total Amount in Pending Today</Text>}
+            SecondText={<Text>$20,541.45</Text>}
+            HeightIcon={62}
+            WidthIcon={60}
+            Choice={5}
+            SecondTextSize={20}
+            ThirdText={<Text>AMT</Text>}
+            FourthText={<Text>48</Text>}
+          />
+          <DashboardTables
+            testfigure={Figures.totalAmountCancelled}
+            FirstText={<Text>Total Amount in Cancelled</Text>}
+            SecondText={<Text>150</Text>}
+            HeightIcon={52}
+            WidthIcon={50}
+            Choice={1}
+            SecondTextSize={55}
+          />
         </View>
       </ScrollView>
       {
@@ -123,7 +136,6 @@ function Dashboard({ navigation }) {
       }
       <View style={styles.containerHeader1}>
         <DashboardTables
-          testfigure={Figures.Vehicle}
           FirstText={<Text>Cars Pending Confirmation</Text>}
           SecondText={<Text>Invoice</Text>}
           HeightBig={65}
@@ -134,13 +146,15 @@ function Dashboard({ navigation }) {
         />
 
         <DashboardTables
-          testfigure={Figures.Vehicle}
+          testfigure={Figures.totalAmountAppointments}
           FirstText={<Text>Cars Pending Confirmation</Text>}
           SecondText={<Text>Invoice</Text>}
           HeightBig={270}
           WidthBig={270}
           Choice={4}
-          margin={0}
+          margin={5}
+          testfigure2={Figures.totalAmountTasks}
+          SecondTextSize={15}
         />
       </View>
     </View>
