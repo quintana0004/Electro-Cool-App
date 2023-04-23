677b6c616e7cfade10cf5f9bf1e7c99e6034b65c
JanMichaelSe <janm.montalvo@gmail.com>
2023-04-23 09:53:58 -0400

    Added the Invoice Items modal to the Dashboard with its corresponding components and made all the backend connections for it. Additionally, changed some styles so all the items would fit.

--- Frontend/components/DashboardInvoice/DashboardInvoiceTableList.js
+++ Frontend/components/DashboardInvoice/DashboardInvoiceTableList.js
@@ -1,44 +1,43 @@
-import {FlatList, StyleSheet, View} from "react-native";
-import {useQuery} from "@tanstack/react-query";
-import {httpGetAllPendingInvoices} from "../../api/invoices.api";
+import { FlatList, StyleSheet, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { httpGetAllPendingInvoices } from "../../api/invoices.api";
 import DashboardInvoiceTableListItem from "./DashboardInvoiceTableListItem";
-import {useInvoiceStore} from "../../Store/invoiceStore";
+import { useInvoiceStore } from "../../Store/invoiceStore";
 
 function DashboardInvoiceTableList({ setSelectedInvoiceId, setModalVisible }) {
   const reloadInvoiceList = useInvoiceStore((state) => state.reloadInvoiceList);
 
-    const {isLoading, isError, data} = useQuery({
-        queryKey: ["DashboardInvoicesData", reloadInvoiceList],
-        queryFn: getInvoicesData,
-        enabled: true,
-    })
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["DashboardInvoicesData", reloadInvoiceList],
+    queryFn: getInvoicesData,
+    enabled: true,
+  });
 
-    async function getInvoicesData() {
-        const response = await httpGetAllPendingInvoices();
-        console.log("INVOICES DATA FROM CARS PENDING CONFIRMATION: ", response.data);
-        return response.data;
-    }
-
-    function renderTableItem({ item }) {
-
-        return (
-          <DashboardInvoiceTableListItem
-            invoiceId={item.id}
-            clientName={`${item.customer.lastName}, ${item.customer.firstName}`}
-            setModalVisible={setModalVisible}
-            setSelectedInvoiceId={setSelectedInvoiceId}
-          />
-        );
-    }
-
-    return (
-      <View style={styles.container}>
-          <FlatList
-            data={data}
-            renderItem={renderTableItem}
-          />
-      </View>
+  async function getInvoicesData() {
+    const response = await httpGetAllPendingInvoices();
+    console.log(
+      "INVOICES DATA FROM CARS PENDING CONFIRMATION: ",
+      response.data
     );
+    return response.data;
+  }
+
+  function renderTableItem({ item }) {
+    return (
+      <DashboardInvoiceTableListItem
+        invoiceId={item.id}
+        clientName={`${item.customer.lastName}, ${item.customer.firstName}`}
+        setModalVisible={setModalVisible}
+        setSelectedInvoiceId={setSelectedInvoiceId}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      {isLoading || <FlatList data={data} renderItem={renderTableItem} />}
+    </View>
+  );
 }
 
 export default DashboardInvoiceTableList;
@@ -49,6 +48,6 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingVertical: 10,
     justifyContent: "center",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 });
--- Frontend/components/DashboardInvoiceItem/DashboardInvoiceItemModal.js
+++ Frontend/components/DashboardInvoiceItem/DashboardInvoiceItemModal.js
@@ -1,118 +1,152 @@
-import {Button, Card, Modal, Portal} from "react-native-paper";
-import {StyleSheet, Text, View} from "react-native";
-import {useState} from "react";
+import { useState } from "react";
+import { Button, Card, Modal, Portal } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+
+import { httpGetInvoice } from "../../api/invoices.api";
 import ClientCard from "../UI/ClientCard";
 import CarCard from "../UI/CarCard";
+import DashboardInvoiceItemList from "./DashboardInvoiceItemList";
+import { MaskedText } from "react-native-mask-text";
 
-function DashboardInvoiceItemModal({ invoiceId, modalVisible, setModalVisible }) {
+function DashboardInvoiceItemModal({
+  invoiceId,
+  modalVisible,
+  setModalVisible,
+}) {
+  const [formattedTotalAmount, setFormattedTotalAmount] = useState(0);
+  const [clientInfo, setClientInfo] = useState(null);
+  const [carInfo, setCarInfo] = useState(null);
+  const [invoiceItems, setInvoiceItems] = useState([]);
 
-    // TODO:
-    // * We need to pass the invoice id that was selected from the parent component.
-    // * Query the data in this component from the server - THIS IS THE WINNER
-    // * We pass the data to the list component
+  const { isLoading, isError, error } = useQuery({
+    queryKey: ["DashboardInvoiceItemsData", invoiceId],
+    queryFn: fetchInvoiceData,
+    enabled: !!invoiceId,
+  });
 
-    return (
-        <Portal>
-            <Modal
-                visible={modalVisible}
-                animationType="fade"
-                transparent={true}
-            >
-                <Card style={[styles.modalContainer]}>
-                    <View style={{ flexDirection: "column" }}>
-                        <Text
-                            style={{
-                                alignSelf: "center",
-                                fontWeight: "bold",
-                                fontSize: 25,
-                            }}
-                        >
-                            Invoice
-                        </Text>
-                        <Card.Actions style={{ alignSelf: "center" }}>
-                            <Button
-                                buttonColor="#E5B126"
-                                textColor="black"
-                                borderColor="#E5B126"
-                                mode="contained"
-                                style={[
-                                    {
-                                        borderRadius: 20,
-                                    },
-                                ]}
-                            >
-                              #{invoiceId}
-                            </Button>
-                        </Card.Actions>
-                    </View>
-                    {/*<View style={{ flexDirection: "row" }}>*/}
-                    {/*    <ClientCard client={clientInfo} />*/}
-                    {/*    <View style={{ marginLeft: 10 }}>*/}
-                    {/*        <CarCard car={carInfo} />*/}
-                    {/*    </View>*/}
-                    {/*</View>*/}
-                    {
-                        //en esta seccion se encuentra el boton del modal que nos da el total
-                    }
-                    {/*<InvoiceDetailTableList invoiceItems={data} />*/}
-                    <Card.Actions style={{ alignSelf: "center" }}>
-                        <Button
-                            buttonColor="#A9D9C2"
-                            textColor="black"
-                            borderColor="#A9D9C2"
-                            mode="contained"
-                            style={[
-                                {
-                                    borderRadius: 20,
-                                },
-                            ]}
-                        >
-                            Total: $500.36
-                        </Button>
-                    </Card.Actions>
-                    {
-                        //en esta seccion se encuentra el boton del modal que dicen cancel y confirmation
-                    }
-                    <Card.Actions>
-                        <Button
-                            title="Cancel"
-                            onPress={() => setModalVisible(false)}
-                            buttonColor="#C4E2E2"
-                            textColor="#138A8C"
-                            borderColor="#138A8C"
-                            mode="contained"
-                            style={[{ borderRadius: 10 }]}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            title="Confirmation"
-                            onPress={() => setModalVisible(false)}
-                            buttonColor="#138A8C"
-                            style={[{ borderRadius: 10 }]}
-                        >
-                            Confirmation
-                        </Button>
-                    </Card.Actions>
-                </Card>
-            </Modal>
-        </Portal>
-    );
+  async function fetchInvoiceData() {
+    const response = await httpGetInvoice(invoiceId);
+    setInvoiceInfo(response.data);
+    return response.data;
+  }
+
+  function setInvoiceInfo(data) {
+    const totalAmount = data.amountTotal * 100;
+    setFormattedTotalAmount(totalAmount);
+    setClientInfo(data.customer);
+    setCarInfo(data.car);
+    setInvoiceItems(data.invoiceItems);
+  }
+
+  return (
+    <Portal>
+      <Modal
+        visible={!isLoading && modalVisible}
+        animationType="fade"
+        transparent={true}
+      >
+        <Card style={[styles.modalContainer]}>
+          <View style={styles.headerContainer}>
+            <Card.Actions style={{ alignSelf: "center" }}>
+              <View style={styles.headerTextContainer}>
+                <Text style={styles.headerText}>Invoice #{invoiceId}</Text>
+              </View>
+            </Card.Actions>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <ClientCard client={clientInfo} />
+            <View style={{ marginLeft: 10 }}>
+              <CarCard car={carInfo} />
+            </View>
+          </View>
+
+          <DashboardInvoiceItemList invoiceItems={invoiceItems} />
+
+          <View>
+            <Card.Actions style={{ alignSelf: "center" }}>
+              <View style={styles.totalAmountContainer}>
+                <Text style={{ fontSize: 18 }}>Total: </Text>
+                <MaskedText
+                  type="currency"
+                  options={{
+                    prefix: "$",
+                    decimalSeparator: ".",
+                    groupSeparator: ",",
+                    precision: 2,
+                  }}
+                  style={{ fontSize: 18 }}
+                >
+                  {formattedTotalAmount}
+                </MaskedText>
+              </View>
+            </Card.Actions>
+            <Card.Actions>
+              <Button
+                title="Cancel"
+                onPress={() => setModalVisible(false)}
+                buttonColor="#C4E2E2"
+                textColor="#138A8C"
+                borderColor="#138A8C"
+                mode="contained"
+                style={[{ borderRadius: 10 }]}
+              >
+                Cancel
+              </Button>
+              <Button
+                title="Confirmation"
+                onPress={() => setModalVisible(false)}
+                buttonColor="#138A8C"
+                style={[{ borderRadius: 10 }]}
+              >
+                Confirmation
+              </Button>
+            </Card.Actions>
+          </View>
+        </Card>
+      </Modal>
+    </Portal>
+  );
 }
 
 export default DashboardInvoiceItemModal;
 
 const styles = StyleSheet.create({
-    modalContainer: {
-      backgroundColor: "white",
-      borderColor: "#cccccc",
-      borderWidth: 4,
-      borderRadius: 20,
-      padding: 20,
-      width: 500,
-      height: 500,
-      alignSelf: "center",
-      justifyContent: "center",
-      alignItems: "center",
-    }
+  modalContainer: {
+    backgroundColor: "white",
+    borderColor: "#cccccc",
+    borderWidth: 4,
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    width: 550,
+    height: 600,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  headerTextContainer: {
+    backgroundColor: "#E5B126",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+  totalAmountContainer: {
+    backgroundColor: "#A9D9C2",
+    borderRadius: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+  },
 });
